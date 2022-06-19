7c9f11203d3d214e9ec28fbdf9da744103593be1
NiNoX14 <hardstuckd5lul@gmail.com>
2022-06-19 09:15:42 +0300

    feat: basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import 'dotenv/config';
+import { createServer } from 'http';
+import { DEFAULT_PORT } from './constants.js';
+
+const PORT = process.env.PORT || DEFAULT_PORT;
+const server = createServer((req, res) => {
+  console.log(req, res);
+});
+
+server.listen(PORT, () => {
+  console.log('Hello World!', PORT);
+});
